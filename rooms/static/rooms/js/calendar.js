d61c24db43e509b04e1f8387768c85112e0f0029
flexirexi <felix_lehmann@ymail.com>
2025-08-02 03:02:44 +0200

    feat: add proper stylings to the booking page

--- rooms/static/rooms/js/calendar.js
+++ rooms/static/rooms/js/calendar.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('.handle').addEventListener('click', () => {
         const slide = document.getElementById('mobile-slideup');
         const up = slide.style.transform === 'translateY(30%)';
-        slide.style.transform = up ? 'translateY(80%)' : 'translateY(30%)';
+        slide.style.transform = up ? 'translateY(85%)' : 'translateY(30%)';
     });
 
     document.getElementById("myDatePicker").addEventListener("change", function() {

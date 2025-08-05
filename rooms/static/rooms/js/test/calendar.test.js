c402f6dee6c539d5812d3266ec0dff867030969a
flexirexi <felix_lehmann@ymail.com>
2025-08-06 01:46:19 +0200

    test: add and pass all 7 tests for rooms app calendar.test.js

--- rooms/static/rooms/js/test/calendar.test.js
+++ rooms/static/rooms/js/test/calendar.test.js
@@ -43,4 +43,51 @@ describe("calendar-utils functions test suite", () => {
         expect(document.getElementById("booking-submit").disabled).toBe(true);
         expect(document.getElementById("form-error-msg").innerHTML).toContain("overlaps");
     });
+
+    test("booking_blocked_on_first_touch shows up the message/hint about the second tap", () => {
+        document.body.innerHTML = `
+            <button id="booking-submit"></button>
+            <div id="form-error-msg"></div>
+            <button id="booking-submit-mobile"></button>
+            <div id="form-error-msg-mobile"></div>
+        `;
+        booking_blocked_on_first_touch();
+        expect(document.getElementById("form-error-msg").innerHTML).toContain("end time slot");
+    });
+
+    test("formatStart creates correct time stamp", () => {
+        document.body.innerHTML = `
+            <input id="myDatePicker" value="2025-12-01">
+        `;
+        const result = formatStart(5);
+        expect(result).toBe("2025-12-01T02:30");
+    });
+
+    test("formatEnd creates a time stamp 30 minutes later", () => {
+        document.body.innerHTML = `
+            <input id="myDatePicker" value="2025-12-01">
+        `;
+        const result = formatEnd(5);
+        expect(result).toBe("2025-12-01T03:00");
+    });
+
+    test("correct values in updateForms", () => {
+        document.body.innerHTML = `
+            <input id="myDatePicker" value="2025-12-01">
+            <input id="id_start">
+            <input id="id_end">
+            <input id="id_room">
+            <input id="id_room_fake">
+            <input id="id_start-mobile">
+            <input id="id_end-mobile">
+            <input id="id_room-mobile">
+            <input id="id_room_fake-mobile">
+        `;
+        updateForms(4, 5, "R1");
+
+        expect(document.getElementById("id_start").value).toBe("2025-12-01T02:00");
+        expect(document.getElementById("id_end").value).toBe("2025-12-01T03:00");
+        expect(document.getElementById("id_room").value).toBe("R1");
+        expect(document.getElementById("id_room_fake-mobile").value).toBe("R1");
+    });
 });
